14db205a7c45ce405d4e1cec246953b58933bf88
Nihar Kartikeya <127834591+Skill0issue@users.noreply.github.com>
2023-08-01 12:28:14 +0530

    validation changes

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -2,9 +2,6 @@ import React, { useState } from 'react'
 import Multiselect from "multiselect-react-dropdown";
 
 const Profile = () => {
-    const [Events, setEvents] = useState();
-    const [Competitions, setCompetitions] = useState();
-
     // options Data set import from the backend 
   
     const options = [
@@ -22,10 +19,10 @@ const Profile = () => {
     //User Details Import from Backend
   
     const [User, setUser] = useState({
-      name: "Nihar Kartikeya",
-      email: "ep22btech11005@iith.ac.in",
-      tel: "",
-      block: "",
+      name: 'Nihar Kartikeya',
+      email: 'ep22btech11005@iith.ac.in',
+      tel: '',
+      block: '-1',
       events: [],
       competitions: [],
     });
@@ -38,52 +35,76 @@ const Profile = () => {
         return { ...prev, [name]: value };
       });
     };
-  
-    // Upon submitting the form
-    function sort_by_Id(){
-      return function (elem1, elem2) {
-        if (elem1.id < elem2.id) {
-          return -1;
-        } else if (elem1.id > elem2.id) {
-          return 1;
-        } else {
-          return 0;
-        }
-      };
-    }
 
 
-    
-  
-    const handleSubmit = (e) => {
-      User.events = Events;
-      User.competitions = Competitions;
-      Competitions.sort(sort_by_Id());
-      Events.sort(sort_by_Id());
-     
-      e.preventDefault();
-  
-      if (User.name === "") {
-        alert("Please provide your name!");
-        document.myForm.name.focus();
-        return false;
-      }
-      if (User.tel === "" || isNaN(User.tel) || User.tel.length !== 10) {
-        alert('Please provide your phone number in the format "xxxxxxxxxx" ');
-        document.myForm.tel.focus();
-        return false;
-      }
-      if (document.myForm.block.value === "-1") {
-        alert("Please provide your Block Name!");
-        return false;
-      }
-      if (User.competitions === undefined || User.events === undefined) {
-        alert("It is required you choose atleast one option from Events & Competitions");
-        return false
-      }
-      return console.log(User);
+    const handleEventsSelect = (selectedList, selectedItem) => {
+      setUser((prevUser) => ({ ...prevUser, events: selectedList }));
     };
   
+    const handleEventsRemove = (selectedList, removedItem) => {
+      setUser((prevUser) => ({ ...prevUser, events: selectedList }));
+    };
+  
+    const handleCompetitionsSelect = (selectedList, selectedItem) => {
+      setUser((prevUser) => ({ ...prevUser, competitions: selectedList }));
+    };
+  
+    const handleCompetitionsRemove = (selectedList, removedItem) => {
+      setUser((prevUser) => ({ ...prevUser, competitions: selectedList }));
+    };
+  
+    // Upon submitting the form     
+  
+      const handleSubmit = (e) => {
+        e.preventDefault();
+    
+        if (!validateName(User.name)) {
+          alert('Please enter a valid name.');
+          return;
+        }
+    
+        if (!validateEmail(User.email)) {
+          alert('Please enter a valid email address.');
+          return;
+        }
+    
+        if (!validatePhoneNumber(User.tel)) {
+          alert('Please provide a valid phone number.');
+          return;
+        }
+    
+        if (User.block === '-1') {
+          alert('Please select a block.');
+          return;
+        }
+    
+        if (User.competitions === undefined || User.events === undefined) {
+          alert('Please choose at least one option from Events & Competitions.');
+          return;
+        }
+    
+        // Form validation passed, proceed with form submission
+
+        User.events.sort(sortByOptionId);
+        User.competitions.sort(sortByOptionId);
+        console.log(User);
+      };
+
+      const sortByOptionId = (option1, option2) => {
+        return option1.id - option2.id;
+      };
+    
+      const validateName = (name) => {
+        return /^[A-Za-z\s]+$/.test(name);
+      };
+    
+      const validateEmail = (email) => {
+        return /\S+@\S+\.\S+/.test(email);
+      };
+    
+      const validatePhoneNumber = (tel) => {
+        return /^[0-9]{10}$/.test(tel);
+      };
     // fetching data
   
     // useEffect(() => {
@@ -99,123 +120,105 @@ const Profile = () => {
     //   };
     // }, []);
   return (
-    <div>
-       <div>
-        <div>
-          <h1 className="p-8 text-xl font-bold uppercase "> Profile Details</h1>
-          <form
-            className="container mx-auto"
-            onSubmit={handleSubmit}
-            name="myForm"
-          >
-            <div className="w-full">
-              <label htmlFor="name" className="form-label">
-                Name
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                className="w-1/4 inputborder"
-                onChange={handleChange}
-                value={User.name}
-              />
-              <label htmlFor="Email" className="form-label">
-                Email
-              </label>
-              <input
-                type="text"
-                name="Email"
-                id="Email"
-                className="w-1/4 inputborder"
-                value={User.email}
-                disabled
-              />
-            </div>
-            <label htmlFor="tel" className="form-label">
-              Phone Number
-            </label>
-            <input
-              type="tel"
-              name="tel"
-              id="tel"
-              className="w-1/4 inputborder"
-              onChange={handleChange}
-            />
-            <label htmlFor="block" className="form-label">
-              Block
-            </label>
-            <select
-              name="block"
-              id="block"
-              className="inputborder"
-              onChange={handleChange}
-            >
-              <option value="-1" defaultChecked>
-                Select Below
-              </option>
-              <option value="A">Aryabhatha</option>
-              <option value="B1">Bhabha</option>
-              <option value="B2">Bhaskhara</option>
-            </select>
-            <div className="container flex w-full gap-4 basis-1/2 justify-evenly">
-              <div className="w-1/3">
-                <Multiselect
-                  name="events"
-                  id="events"
-                  isObject={true}
-                  placeholder="Select Events"
-                  options={options} // Options to display in the dropdown
-                  onSelect={setEvents}
-                  onRemove={setEvents}
-                  displayValue="name" // Property name to display in the dropdown options
-                  showCheckbox
-                  className="border-gray-500 inputborder"
-                  showArrow
-                  style={{
-                    multiselectContainer: { width: "100%", height: "auto" },
-                    optionContainer: { border: "none" },
-                    chips: { background: "red" },
-                    searchBox: {
-                      border: "2px",
-                      borderBottom: "1px solid blue",
-                      borderRadius: "0px",
-                    },
-                  }}
-                />
-              </div>
-              <div className="w-1/3">
-                <Multiselect
-                  name="competitions"
-                  id="competitions"
-                  isObject={true}
-                  placeholder="Select Competitons"
-                  options={options} // Options to display in the dropdown
-                  onSelect={setCompetitions}
-                  onRemove={setCompetitions}
-                  displayValue="name" // Property name to display in the dropdown options
-                  showCheckbox
-                  className="border-gray-500 inputborder"
-                  showArrow
-                  style={{
-                    multiselectContainer: { width: "100%", height: "auto" },
-                    optionContainer: { border: "none" },
-                    chips: { background: "red" },
-                    searchBox: {
-                      border: "2px",
-                      borderBottom: "1px solid blue",
-                      borderRadius: "0px",
-                    },
-                  }}
-                />
-              </div>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
+    <div className="container">
+      <h1 className="p-8 text-xl font-bold uppercase ">Profile Details</h1>
+      <form onSubmit={handleSubmit} name="myForm">
+        <div className="form-group">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="inputborder"
+            onChange={handleChange}
+            value={User.name}
+          />
         </div>
+        <div className="form-group">
+          <label htmlFor="Email" className="form-label">
+            Email
+          </label>
+          <input
+            type="text"
+            name="Email"
+            id="Email"
+            className="inputborder"
+            value={User.email}
+            disabled
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="tel" className="form-label">
+            Phone Number
+          </label>
+          <input
+            type="tel"
+            name="tel"
+            id="tel"
+            className="inputborder"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="block" className="form-label">
+            Block
+          </label>
+          <select
+            name="block"
+            id="block"
+            className="inputborder"
+            onChange={handleChange}
+          >
+            <option value="-1" defaultChecked>
+              Select Below
+            </option>
+            <option value="A">Aryabhatha</option>
+            <option value="B1">Bhabha</option>
+            <option value="B2">Bhaskhara</option>
+          </select>
+        </div>
+
+      <div className="container flex w-full gap-4 basis-1/2 justify-evenly">
+      <div className="w-1/3">
+        <Multiselect
+          name="events"
+          id="events"
+          isObject={true}
+          placeholder="Select Events"
+          options={options}
+          onSelect={handleEventsSelect}
+          onRemove={handleEventsRemove}
+          displayValue="name"
+          showCheckbox
+          className="border-gray-500 inputborder"
+          showArrow
+        />
       </div>
-    </div>
-  )
+      <div className="w-1/3">
+        <Multiselect
+          name="competitions"
+          id="competitions"
+          isObject={true}
+          placeholder="Select Competitons"
+          options={options}
+          onSelect={handleCompetitionsSelect}
+          onRemove={handleCompetitionsRemove}
+          displayValue="name"
+          showCheckbox
+          className="border-gray-500 inputborder"
+          showArrow
+        />
+      </div>
+            </div>
+            <button type="submit" onSubmit={handleSubmit}>Submit</button>
+    </form>
+  </div>
+  );
 }
 
 export default Profile
+
+
+
